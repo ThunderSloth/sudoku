94d647636748f95512beca5b7b6f6407c67f2e5a
Eli Bell <elibell@elibell.net>
2023-11-13 01:58:24 -0500

    dunder str

--- solver.py
+++ solver.py
@@ -17,6 +17,7 @@ class Origin(Node):
 
 
 class Header(Node):
+    keys = {v: i for i, v in enumerate(["row", "col", "box", "num"])}
     def __init__(self, data):
         super().__init__()
         self.data = []
@@ -25,14 +26,24 @@ class Header(Node):
 
 
 class Constraint(Header):
+    index = 0
     def __init__(self, group, data):
         super().__init__(data)
         self.group = group
+        self.index = Constraint.index
+        Constraint.index += 1
 
 
 class Option(Header):
+    index = 0
     def __init__(self, data):
         super().__init__(data)
+        self.index = Option.index
+        Option.index += 1
+
+    def __str__(self):
+        return ", ".join([f"{k}: {v}" for k, v in zip(Option.keys.keys(), self.data)])
+            
 
 
 class Table:
@@ -42,6 +53,20 @@ class Table:
         self.origin.right = self.origin
         self.origin.down = self.origin
         self.origin.left = self.origin
+        self.options = {}
+        self.givens = []
+        self.grid = None
+
+    def __str__(self):
+        result = ""
+        if self.grid is not None:
+            for row in self.grid:
+                for cell in row:
+                    result += " " if cell is None else str(cell)
+                result += "\n"
+        return result
+
+
 
     def add_constraint(self, group, data):
         constraint = Constraint(group, data)
@@ -51,6 +76,7 @@ class Table:
         self.origin.left = constraint
 
     def add_option(self, data):
+        # This function depends on all constraints being added first!
         option = Option(data)
         self.origin.up.down = option
         option.up = self.origin.up
@@ -60,6 +86,9 @@ class Table:
         while constraint is not self.origin:
             self.add_cell(constraint, option)
             constraint = constraint.right
+        row, col, box, num = data
+        # box unnecessary for look-up
+        self.options[(row, col, num)] = option
 
     def remove_constraint(self, constraint):
         current = constraint
@@ -99,7 +128,14 @@ class Table:
         cell.left = option.left
         cell.right = option
         option.left = cell
-
+        if self.grid is None:
+            self.grid = [[None] * Constraint.index]
+        while len(self.grid) <= option.index:
+            self.grid.append([None] * Constraint.index)
+        self.grid[option.index][constraint.index] = option.data[Option.keys["num"]]
+    
+    def add_given(self, *ref):
+        self.givens.append(self.options[tuple(ref)])     
 
 class Sudoku:
     def __init__(self):
@@ -112,7 +148,7 @@ class Sudoku:
             size = len(vector)
             items = [i for i in range(size) if int(vector[i])]
             for i in range(9**2):
-                n = divmod(i, 9)
+                n = [v + 1 for v in divmod(i, 9)]
                 data = [None] * size
                 data[items[0]] = n[0]
                 data[items[1]] = n[1]
@@ -122,8 +158,31 @@ class Sudoku:
             row, col = divmod(i, 9)
             for num in range(9):
                 box = 3 * (col // 3) + (row // 3)
-                data = [row, col, box, num]
+                data = [v + 1 for v in [row, col, box, num]]
                 self.table.add_option(data)
 
+    def solve(self, puzzle):
+        for row, arr in enumerate(puzzle, start = 1):
+            for col, num in enumerate(arr, start = 1):
+                if num:
+                    self.table.add_given(row, col, num)
+        print(self.table)
 
-Sudoku()
+
+
+sudoku = Sudoku()
+
+puzzle = [[5,3,0,0,7,0,0,0,0],
+          [6,0,0,1,9,5,0,0,0],
+          [0,9,8,0,0,0,0,6,0],
+          [8,0,0,0,6,0,0,0,3],
+          [4,0,0,8,0,3,0,0,1],
+          [7,0,0,0,2,0,0,0,6],
+          [0,6,0,0,0,0,2,8,0],
+          [0,0,0,4,1,9,0,0,5],
+          [0,0,0,0,8,0,0,7,9]]
+
+def solve(puzzle):
+    sudoku.solve(puzzle)
+                
+solve(puzzle)
