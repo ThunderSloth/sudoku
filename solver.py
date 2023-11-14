eec6fedef39752d5a9c543555684874fd5f8a579
Eli Bell <elibell@elibell.net>
2023-11-13 23:59:13 -0500

    sweet ascii print-out of the exact-cover matrix

--- solver.py
+++ solver.py
@@ -14,16 +14,17 @@ class Node:
 class Origin(Node):
     def __init__(self):
         super().__init__()
+    
 
 
 class Header(Node):
-    keys = {v: i for i, v in enumerate(["row", "col", "box", "num"])}
     def __init__(self, data):
         super().__init__()
-        self.data = []
-        for v in data:
-            self.data.append(v)
-
+        self.data = data
+        self.set = set({(k, v) for k, v in data.items() if v is not None})
+    
+    def __str__(self):
+        return "{{{}}}".format(", ".join([f"{k}: {v}" for k, v in self.data.items()]))
 
 class Constraint(Header):
     index = 0
@@ -33,6 +34,9 @@ class Constraint(Header):
         self.index = Constraint.index
         Constraint.index += 1
 
+    def __str__(self):
+        data = super().__str__()
+        return "{} ({}) = {}".format("Constraint", self.group, data) 
 
 class Option(Header):
     index = 0
@@ -42,8 +46,7 @@ class Option(Header):
         Option.index += 1
 
     def __str__(self):
-        return ", ".join([f"{k}: {v}" for k, v in zip(Option.keys.keys(), self.data)])
-            
+        return "r{}c{}={}".format(*[self.data.get(v) for v in ["row", "col", "num"]])
 
 
 class Table:
@@ -55,18 +58,49 @@ class Table:
         self.origin.left = self.origin
         self.options = {}
         self.givens = []
-        self.grid = None
 
     def __str__(self):
-        result = ""
-        if self.grid is not None:
-            for row in self.grid:
-                for cell in row:
-                    result += " " if cell is None else str(cell)
-                result += "\n"
-        return result
+        elements = ["row", "col", "box", "num"]
+        lhs_width = 7
+        intro = ("This is an exact cover matrix for the game Sudoku. The rows\n"
+                "represent every possible candidate placement, while the columns \n"
+                "represent the constraints derived from the rules of Sudoku.\n"
+                "There are 9x9x9=729 possible candidates and 9x9x4=324 constraints.\n"
+                "The occupied cells indicate where the candidates and constraints\n"
+                "intersect, the numbers displayed are simply the corresponding\n"
+                "candidate numbers for ease of reading.\n")
+        header = ["{:{}}".format(f"{lhs:5s}:", lhs_width) for lhs in ["GROUP"] + elements]
+        sep_index = [lhs_width]
+        constraint = self.origin.right
+        while constraint is not self.origin:
+            if constraint.left == self.origin or constraint.left.group != constraint.group:
+                header = [v + "|" for v in header]
+                header[0] += constraint.group.upper()
+            for i, v in enumerate(elements, start=1):
+                val = constraint.data.get(v)
+                header[i] += " " if val is None else str(val)
+            if constraint.right == self.origin or constraint.right.group != constraint.group:
+                sep_index.append(len(header[1]))
+                header[0] += " " * (len(header[1]) - len(header[0]))
+            constraint = constraint.right
 
-
+        sep_line = "".join(["+" if i in sep_index else "-" for i in range(sep_index[-1])])
+        table = []
+        option = self.origin.down
+        while option is not self.origin:
+            num = option.data.get("num")
+            if num == 1:
+                table.append(f"{sep_line}")
+            lhs = f"{str(option):{lhs_width}s}"
+            table.append(lhs)
+            constraint = self.origin.right
+            while constraint is not self.origin:
+                if len(table[-1]) in sep_index:
+                    table[-1] += "|"
+                table[-1] += str(num) if constraint.set.issubset(option.set) else " "
+                constraint = constraint.right
+            option = option.down
+        return "\n".join([v for v in [intro] + header + table])
 
     def add_constraint(self, group, data):
         constraint = Constraint(group, data)
@@ -84,11 +118,11 @@ class Table:
         self.origin.up = option
         constraint = self.origin.right
         while constraint is not self.origin:
-            self.add_cell(constraint, option)
+            if constraint.set.issubset(option.set):
+                self.add_cell(constraint, option)
             constraint = constraint.right
-        row, col, box, num = data
         # box unnecessary for look-up
-        self.options[(row, col, num)] = option
+        self.options[tuple([data.get(v) for v in ["row", "col", "num"]])] = option
 
     def remove_constraint(self, constraint):
         current = constraint
@@ -128,14 +162,10 @@ class Table:
         cell.left = option.left
         cell.right = option
         option.left = cell
-        if self.grid is None:
-            self.grid = [[None] * Constraint.index]
-        while len(self.grid) <= option.index:
-            self.grid.append([None] * Constraint.index)
-        self.grid[option.index][constraint.index] = option.data[Option.keys["num"]]
-    
+
     def add_given(self, *ref):
-        self.givens.append(self.options[tuple(ref)])     
+        self.givens.append(self.options[tuple(ref)])
+
 
 class Sudoku:
     def __init__(self):
@@ -144,6 +174,7 @@ class Sudoku:
         # bit-vectors indicating the elements that belong to each constraint group,
         # in order: row, column, box, number.
         groups = {"cell": "1100", "row": "1001", "col": "0101", "box": "0011"}
+        elements = ["row", "col", "box", "num"]
         for group, vector in groups.items():
             size = len(vector)
             items = [i for i in range(size) if int(vector[i])]
@@ -152,37 +183,43 @@ class Sudoku:
                 data = [None] * size
                 data[items[0]] = n[0]
                 data[items[1]] = n[1]
-                self.table.add_constraint(group, data)
+                self.table.add_constraint(group, {k: v for k, v in zip(elements, data)})
 
         for i in range(9**2):
-            row, col = divmod(i, 9)
-            for num in range(9):
-                box = 3 * (col // 3) + (row // 3)
-                data = [v + 1 for v in [row, col, box, num]]
+            d = {}
+            d["row"], d["col"] = divmod(i, 9)
+            for d["num"] in range(9):
+                d["box"] = 3 * (d.get("col") // 3) + (d.get("row") // 3)
+                data = {k: v + 1 for k, v in d.items()} 
                 self.table.add_option(data)
 
     def solve(self, puzzle):
-        for row, arr in enumerate(puzzle, start = 1):
-            for col, num in enumerate(arr, start = 1):
+        for row, arr in enumerate(puzzle, start=1):
+            for col, num in enumerate(arr, start=1):
                 if num:
                     self.table.add_given(row, col, num)
-        print(self.table)
 
+        with open("DLX.txt", "w") as DLX:
+            DLX.write(f"{self.table}")
 
 
 sudoku = Sudoku()
 
-puzzle = [[5,3,0,0,7,0,0,0,0],
-          [6,0,0,1,9,5,0,0,0],
-          [0,9,8,0,0,0,0,6,0],
-          [8,0,0,0,6,0,0,0,3],
-          [4,0,0,8,0,3,0,0,1],
-          [7,0,0,0,2,0,0,0,6],
-          [0,6,0,0,0,0,2,8,0],
-          [0,0,0,4,1,9,0,0,5],
-          [0,0,0,0,8,0,0,7,9]]
+puzzle = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9],
+]
+
 
 def solve(puzzle):
     sudoku.solve(puzzle)
-                
+
+
 solve(puzzle)
